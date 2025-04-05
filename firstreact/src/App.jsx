07175e8bd00460f7a417b4af26dae49d613727d8
Shubhashh <shubhash.pisike69@gmail.com>
2025-04-05 10:24:47 +0530

    commit

---
+++ firstreact/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './App.css'
+
+
+ let a= "Apples"
+
+let b="Banana"
+
+
+function App() {
+  return (
+    <div className='container'>
+      <title>Shubhash</title>
+      <h1 className='head'>Heading</h1>
+      {/* <h1>I Like {a} , {b}</h1> */}
+      
+  <p  className='para'>This is Paragraph</p>
+  
+  <img src="https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630" class="center-img"  />
+<br/>
+<img src="Picture/image.jpg" alt="Example Image" className="samplePicture"/>
+
+    </div>
+  )
+}
+
+export default App
